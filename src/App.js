667be8ac6f0223a2c9f45e3248dac689fb991f46
adamochi <honeyrumballs@gmail.com>
2022-06-07 14:03:21 +1000

    css test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.css";
 import Header from "./components/Header";
 import React from "react";
 
 function App() {
   const name = "adam";
   return (
-    <div className="container">
+    <div className={styles.App_header}>
       <h1>Hello {name}</h1>
       {Header}
-      <img src={logo} />
+      <img src={logo} className={styles.App - logo} alt="logo" />
     </div>
   );
 }
