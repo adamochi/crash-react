5af68b3c3b4fc4b406423bcb12720cb5914843ca
adamochi <honeyrumballs@gmail.com>
2022-06-07 13:51:00 +1000

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import React from "react";
 
 function App() {
   const name = "adam";
   return (
     <div className="container">
       <h1>Hello {name}</h1>
+      {Header}
       <img src={logo} />
     </div>
   );
