c227a1415a241b787cc1111265c5ca1b8dde135c
adamochi <honeyrumballs@gmail.com>
2022-06-08 14:08:49 +1000

    readme

--- src/App.js
+++ src/App.js
@@ -5,36 +5,36 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Tasks from "./components/Tasks";
 
-
 function App() {
   const name = "adamochi";
   useEffect(() => {
-    const script = document.createElement('script');
-  
-    script.src = "https://kit.fontawesome.com/a7199961f0.js" 
-    script.crossorigin="anonymous"
+    const script = document.createElement("script");
+
+    script.src = "https://kit.fontawesome.com/a7199961f0.js";
+    script.crossorigin = "anonymous";
     script.async = true;
-  
     document.body.appendChild(script);
-  
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
   return (
     <>
-    
-    <div className={style.App_header}>
-    <div className={style.nav_bar}>
-    <span><i class="fa-brands fa-github-alt"></i> Source</span>
-    </div>
-      <h1>Hello {name}</h1>
-      <Header />
-      <Tasks />
-      <div className={style.low_go}>
-        <img src={logo} className={style.App_logo} alt="logo" />
+      <div className={style.nav_bar}>
+        <a target="_blank" href="https://github.com/adamochi">
+          <span>
+            <i class="fa-brands fa-github-alt fa-shake"></i> Source
+          </span>
+        </a>
       </div>
-    </div>
+      <div className={style.App_header}>
+        <h1>Hello {name}</h1>
+        <Header />
+        <Tasks />
+        <div className={style.low_go}>
+          <img src={logo} className={style.App_logo} alt="logo" />
+        </div>
+      </div>
     </>
   );
 }
