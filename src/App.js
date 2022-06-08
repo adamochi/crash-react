07a7fe5ef6656caeaa2c11f22f75011ea93b3dea
adamochi <honeyrumballs@gmail.com>
2022-06-08 11:06:57 +1000

    font awesome

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from "./logo.svg";
 import style from "./App.module.css";
 import Header from "./components/Header";
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Tasks from "./components/Tasks";
 
+
 function App() {
-  const name = "adam";
+  const name = "adamochi";
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://kit.fontawesome.com/a7199961f0.js" 
+    script.crossorigin="anonymous"
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
   return (
+    <>
+    
     <div className={style.App_header}>
+    <div className={style.nav_bar}>
+    <span><i class="fa-brands fa-github-alt"></i> Source</span>
+    </div>
       <h1>Hello {name}</h1>
       <Header />
       <Tasks />
@@ -16,6 +35,7 @@ function App() {
         <img src={logo} className={style.App_logo} alt="logo" />
       </div>
     </div>
+    </>
   );
 }
 
